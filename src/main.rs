11a1eac4837fe9faafa16f47844c93c27eed779c
kw-andy <andy.kwok.work@gmail.com>
2020-06-19 15:19:30 +0200

    adding a README.MD and all the files from the previous lessons

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,15 @@
-/* 
+/*
 
 https://www.youtube.com/watch?v=udrjtjKuVh8&list=PLVvjrrRCBy2JSHf9tGxGKJ-bYAN_uDCUL&index=14
 
-// 20 imple key
-
 */
 
+use rand::Rng;
 
-//struct alawas outside of the function?
 
-struct Rectangle {
 
-    width: u32,
-    height:u32
-
-}
-
-impl Rectangle {
-
-    fn print_description(&self) {
-        println!("Rectangle {} x {}",self.width,self.height);
-
-    }
-
-    fn is_square(&self) -> bool {
-
-        self.width == self.height
-    }
-
-}
 
 fn main() {
-
-    let my_rect = Rectangle {width : 10, height: 20};
-
-    my_rect.print_description();
-    
-    println!("my rectangle is {}",my_rect.is_square());
-
-
+    println!("{}","je fais du rust");
 }
 
